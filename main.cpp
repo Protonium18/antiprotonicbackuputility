7aaa0fe0c0484de1d51b4a0e20f7c5f4094e05e7
Protonium18 <37570772+Protonium18@users.noreply.github.com>
2021-09-26 19:40:40 -1000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <string>
 
 namespace fs = std::filesystem;
 
@@ -9,6 +10,7 @@ struct path_pair {
 	std::string dst;
 
 };
+
 
 void file_open(const std::string* in_path, const std::string* dest_path) {
 	std::ifstream src;
@@ -27,7 +29,9 @@ void file_open(const std::string* in_path, const std::string* dest_path) {
 
 int dir_copy(const std::string* in_path, const std::string* out_path, bool overwrite) {
 
-	std::string newpath = *out_path + "/" + *in_path;
+	int index(in_path->rfind("/"));
+	std::string end = in_path->substr(index + 1);
+	std::string newpath = *out_path + "/" + end;
 	
 	auto copyoptions = fs::copy_options::recursive;
 
@@ -60,9 +64,43 @@ int dir_copy(const std::string* in_path, const std::string* out_path, bool overw
 
 }
 
-void loadfromtxt(std::string path) {
+int dir_copy(const path_pair* path, const bool overwrite) {
 
+	std::string in_path = path->src;
+	std::string out_path = path->dst;
 
+	int index(in_path.rfind("/"));
+	std::string end = in_path.substr(index + 1);
+	std::string newpath = out_path + "/" + end;
+
+	auto copyoptions = fs::copy_options::recursive;
+
+	if (overwrite) {
+		copyoptions = fs::copy_options::recursive | fs::copy_options::overwrite_existing;
+	}
+
+	if (!fs::exists(newpath)) {
+		fs::create_directories(newpath);
+	}
+	else if (fs::exists(newpath) && overwrite == false) {
+		std::cout << "Path already exists! Overwrite? [y/n]" << std::endl;
+		char input;
+		std::cin >> input;
+		if (input == 'y') {
+			copyoptions = fs::copy_options::recursive | fs::copy_options::overwrite_existing;
+		}
+		else if (input == 'n') {
+			std::cout << "Skipped!" << std::endl;
+			return 1;
+		}
+		else {
+			std::cout << "Bad input" << std::endl;
+			return 1;
+		}
+	}
+
+	fs::copy(in_path, newpath, copyoptions);
+	return 0;
 
 }
 
@@ -84,6 +122,137 @@ path_pair file_dialogue()
 
 }
 
+bool copy_confirm(std::vector<path_pair>* list) {
+
+	std::string cinput;
+	int iinput = 0;
+
+	std::cout << "These files will be copied:" << std::endl;
+	for (int i = 0; i < list->size(); i++) {
+		std::cout << list->at(i).src + " to " + list->at(i).dst << std::endl;
+	}
+	while (true) {
+		std::cout << "Continue? [y/n]" << std::endl;
+		std::cin >> cinput;
+		if (cinput == "y") {
+			return true;
+		}
+		else if (cinput == "n") {
+			std::cout << "Alter entry? [y/n]" << std::endl;
+			std::cin >> cinput;
+			if (cinput == "y") {
+				std::cout << "Which entry?" << std::endl;
+				std::cin >> iinput;
+				path_pair output = file_dialogue();
+				list->at(iinput).src = output.src;
+				list->at(iinput).dst = output.dst;
+			}
+			else if (cinput == "n") {
+				return false;
+			}
+		}
+	}
+}
+
+int loadfromtxt(std::string path) {
+	std::ifstream file_open(path, std::ios_base::in);
+	std::string output;
+	std::vector<std::string> output_vector;
+	std::vector<path_pair>* path_pairs = new std::vector<path_pair>;
+	bool overwrite = false;
+
+	path_pair tmppath;
+
+	if (!file_open.is_open()) {
+		std::cout << "File failed to open.\n";
+		return 0;
+	}
+
+	while (std::getline(file_open, output)) {
+		output_vector.push_back(output);
+
+	}
+
+	int pos = 0;
+
+
+	for (int i = 0; 0 < output_vector.size()/2; i++) {
+		path_pair tmppath;
+
+		if (pos == output_vector.size()) {
+			break;
+		}
+
+		tmppath.src = output_vector.at(pos);
+		tmppath.dst = output_vector.at(pos + 1);
+		
+		path_pairs->push_back(tmppath);
+
+		pos+=2;
+
+	}
+
+	file_open.close();
+
+	if (copy_confirm(path_pairs)) {
+		for (int i = 0; i < path_pairs->size(); i++) {
+			dir_copy(&path_pairs->at(i), overwrite);
+		}
+
+	}
+
+	delete(path_pairs);
+
+}
+
+void single_copy() {
+	path_pair path = file_dialogue();
+	dir_copy(&path, false);
+}
+
+void multi_copy() {
+	int input = 0;
+	int count = 0;
+	bool overwrite = false;
+
+	std::vector<path_pair> path_pairs;
+
+	std::cout << "How many directories to copy?\n";
+	std::cin >> input;
+	while (count < input) {
+		path_pairs.push_back(file_dialogue());
+		count++;
+	}
+	copy_confirm(&path_pairs);
+	for (int i = 0; i < count; i++) {
+		dir_copy(&path_pairs.at(i), overwrite);
+	}
+}
+
+void start_menu() {
+	int input = 0;
+
+	std::cout << "1. Single copy\n";
+	std::cout << "2. Multi-copy\n";
+	std::cout << "3. Load path pairs from .txt\n";
+
+	std::cin >> input;
+
+	if (input == 1) {
+		single_copy();
+	}
+
+	if (input == 2) {
+		multi_copy();
+	}
+
+	if (input == 3) {
+		loadfromtxt("textinput.txt");
+	}
+
+
+}
+
 int main() {
 	
 	int input = 0;
@@ -94,50 +263,27 @@ int main() {
 	std::vector<std::string> list_copy;
 	std::vector<std::string> list_dest;
 
+	std::vector<path_pair> pathpair_list;
+
 
 	std::cout << "AntiProtonic's Backup Utility" << std::endl;
 
+	start_menu();
 
+	//while (input < 2) {
 
-	while (input < 2) {
+	//	path_pair output = file_dialogue();
+	//	pathpair_list.push_back(output);
+	//	input++;
+	//}
 
-		path_pair output = file_dialogue();
-
-		list_copy.push_back(output.src);
-		list_dest.push_back(output.dst);
-		input++;
-	}
-
-	std::cout << "These files will be copied:" << std::endl;
-	for (int i = 0; i < list_copy.size(); i++) {
-		std::cout << i + " " + list_copy.at(i) + " to " + list_dest.at(i) << std::endl;
-	}
-
-	std::cout << "Continue? [y/n]" << std::endl;
-	std::cin >> cinput;
-	if (cinput == "y") {
-		for (int i = 0; i < list_copy.size(); i++) {
-			dir_copy(&list_copy.at(i), &list_dest.at(i), auto_overwrite);
-		}
-	}
-	else if (cinput == "n") {
-		std::cout << "Alter entry? [y/n]" << std::endl;
-		std::cin >> cinput;
-		if (cinput == "y") {
-			std::cout << "Which entry?" << std::endl;
-			std::cin >> iinput;
-			path_pair output = file_dialogue();
-			list_copy.at(iinput) = output.src;
-			list_dest.at(iinput) = output.dst;
-			for (int i = 0; i < list_copy.size(); i++) {
-				dir_copy(&list_copy.at(i), &list_dest.at(i), auto_overwrite);
-			}
-			
- 		}
-		if (cinput == "n") {
-			return 0;
-		}
-	}
+	//		}
+	//		
+ //		}
+	//	if (cinput == "n") {
+	//		return 0;
+	//	}
+	//}
 
 
 	return 0;
